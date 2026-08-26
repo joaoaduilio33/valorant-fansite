1bbc43927d05466769323c84f982d3344d53e632
joaoaduilio33 <hsdfs922@gmail.com>
2026-08-26 13:55:20 -0300

    Add static worker deployment entry

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,17 @@
 import { defineConfig } from 'vite';
 import { sites } from '@openai/sites-vite-plugin';
-export default defineConfig({ plugins: [sites()] });
+
+function staticWorker() {
+  return {
+    name: 'static-site-worker',
+    generateBundle() {
+      this.emitFile({
+        type: 'asset',
+        fileName: 'server/index.js',
+        source: `export default { async fetch(request, env) { return env.ASSETS.fetch(request); } };`,
+      });
+    },
+  };
+}
+
+export default defineConfig({ plugins: [staticWorker(), sites()] });
